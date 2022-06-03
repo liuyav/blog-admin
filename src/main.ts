e6bca343d610bc8fda9d23735f354644f324a0d1
liuyav <2472479696@qq.com>
2022-06-03 10:20:56 +0800

    feature: 集成pinia

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import store from './store'; // 创建vue实例
+const app = createApp(App); // 挂载pinia
 
-createApp(App).mount('#app');
+app.use(store); // 挂载实例
+
+app.mount('#app');
